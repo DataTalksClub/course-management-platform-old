97e3219376cea92e288e930a326d5d3597a9de00
Larisa <larisa@larisas-mbp.lan>
2022-09-06 13:23:48 +0200

    simple website structure added

--- cmp/course_app/urls.py
+++ cmp/course_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.courses , name='courses'),
+    path('course/<str:pk>', views.course, name='course')
 ]
--- cmp/course_app/views.py
+++ cmp/course_app/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello it is a Course App")
+coursesList = [
+    {
+        'id':'1',
+        'title':'Machine Learning Zoomcamp',
+        'description':'Waht, never heard of it',
+    },
+    {
+        'id':'2',
+        'title':'Course n2',
+        'description':'Waht, never heard of it',
+    },
+    {
+        'id':'3',
+        'title':'MLOPs',
+        'description':'Waht, never heard of it',
+    }
+]
+
+def courses(request):
+    context = {'courses': coursesList}
+    return render(request, 'course_app/courses.html', context)
+
+def course(request, pk):
+    courseObj = None
+    for i in coursesList:
+        if i['id'] == pk:
+            courseObj = i
+
+    return render(request, 'course_app/course.html', {'course':courseObj})
 
 
